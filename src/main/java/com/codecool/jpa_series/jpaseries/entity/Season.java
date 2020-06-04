e51cc724f1792c299d409c021f20c81a38284d7a
Gnorbi951 <gnorbi951@gmail.com>
2020-06-04 23:09:57 +0200

    Initialise Season entity

--- src/main/java/com/codecool/jpa_series/jpaseries/entity/Season.java
+++ src/main/java/com/codecool/jpa_series/jpaseries/entity/Season.java
@@ -1,4 +1,25 @@
 package com.codecool.jpa_series.jpaseries.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Season {
+
+    @Id
+    @GeneratedValue
+    private Long Id;
+    private String title;
+    private Integer length;
+
 }
